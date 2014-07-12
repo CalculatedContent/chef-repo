ff729ad709a1797ea2d5ca7ea87d89a1ff83d2bf
Charles H Martin <charlesmartin14@gmail.com>
2014-07-11 20:20:22 -0700

    added port to the attributes

--- cookbooks/simple_endpoint/attributes/default.rb
+++ cookbooks/simple_endpoint/attributes/default.rb
@@ -1,4 +1,5 @@
 default['sinatra_app']['docroot'] = '/home/ubuntu/apps'
+default['sinatra_app']['port'] = 1234
 default['sinatra_app']['name'] = 'simple_endpoint'
 default['sinatra_app']['deploy_dir'] = '/home/ubuntu/apps/simple_endpoint'
 default['sinatra_app']['repository'] = 'git://github.com/CalculatedContent/simple_endpoint.git'
