a4e21ddbfd4a1c306c6aa1c1e87bf4be732b11b5
Charles H Martin <charlesmartin14@gmail.com>
2014-12-26 16:24:19 -0800

    apache modified to run on ubuntu 14

--- cookbooks/cloud-crawler/attributes/default.rb
+++ cookbooks/cloud-crawler/attributes/default.rb
@@ -4,8 +4,9 @@ default['sinatra_app']['port'] = '3456'
 default['sinatra_app']['deploy_dir'] = '/home/ubuntu/apps/cloud-monitor'
 default['sinatra_app']['repository'] = 'git://github.com/CalculatedContent/cloud-monitor.git'
 default['sinatra_app']['repository'] = 'git://github.com/CalculatedContent/cloud-monitor.git'
-default['s3cmd']['aws_access_key'] = 
-default['s3cmd']['aws_secret_key'] = 
+default['s3cmd']['aws_access_key'] = "AKIAJTOTBNUMY67A5G5A"
+default['s3cmd']['aws_secret_key'] = "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"
+
 
 
 
