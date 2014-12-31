e22b2b20063f254727a1b81477f4117f7115fdbd
Charles Martin <charlesmartin14@gmail.com>
2014-12-30 21:23:00 -0800

    Update default.rb

--- cookbooks/s3cmd/attributes/default.rb
+++ cookbooks/s3cmd/attributes/default.rb
@@ -1,7 +1,7 @@
 #list of users that will have the s3cmd configuration 
 default[:s3cmd][:users] = [:ubuntu, :root] 
-default[:s3cmd][:aws_access_key] = "AKIAJFP2EWGB2S3MHPTA"
-default[:s3cmd][:aws_secret_key] = "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"
+default[:s3cmd][:aws_access_key] = ""
+default[:s3cmd][:aws_secret_key] = ""
 
 
 
